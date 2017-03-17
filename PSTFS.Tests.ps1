261368a36b112299d2922aeaa2cf67c10f19931e
Adam Bertram <adbertram@gmail.com>
2017-03-17 15:20:10 -0500

    Small update to Pester tests

--- PSTFS.Tests.ps1
+++ PSTFS.Tests.ps1
@@ -2,7 +2,7 @@
 $ThisModule = "$($MyInvocation.MyCommand.Path -replace '\.Tests\.ps1$', '').psm1"
 $ThisModuleName = (($ThisModule | Split-Path -Leaf) -replace '\.psm1')
 Get-Module -Name $ThisModuleName -All | Remove-Module -Force
-Import-Module -Name $ThisModule -ArgumentList $true -Force -ErrorAction Stop
+Import-Module -Name $ThisModule -Force -ErrorAction Stop
 #endregion
 
 InModuleScope $ThisModuleName {
